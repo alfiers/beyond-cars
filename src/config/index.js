145c40084c54d9c9c198ed7bbb22de27ffcba1eb
user <>
2019-01-24 09:22:10 -0700

    update

---
+++ src/config/index.js
@@ -0,0 +1,12 @@
+export const SLACK_URL =
+  "https://hooks.slack.com/services/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";
+export const SENDGRID_TEMPLATEIDS = [
+  "d-47fe052032604c5e9848be1191fa9830",
+  "d-ca00123f59554fd89931f545592b86ab",
+  "d-7b72a12f13c74e82bee31965ad6e0880",
+  "d-8f6ff151af9e404795f8bf091220d71d",
+  "d-dfa6f124e6db409c8a81549763361e2a",
+  "d-314f420ad1bc452ebd3d5bd8cf7ad593",
+  "d-7775536dc08942c9be40abc17c29f2ee",
+  "d-9568d8fd0ffc4cca8cc2463e9ade388f"
+];
